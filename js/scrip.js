c338b327c04454f74c0dd63e55a64f576cfb1c26
Rafael Moreira <rafaelmoreirap@hotmail.com>
2024-07-16 17:16:14 +0100

    Netflix

---
+++ js/scrip.js
@@ -0,0 +1,34 @@
+const hd = document.getElementById('hd');
+window.addEventListener('scroll',()=>{
+    if(window.scrollY >= 100){
+        hd.classList.add('nav_black');
+    }else{
+        hd.classList.remove('nav_black');
+    }
+})
+
+
+$('.sliders').slick({
+    infinite: true,
+    slidesToShow: 6,
+    slidesToScroll: 2
+});
+
+
+
+var iconMenu = document.querySelectorAll('.icon');
+
+	iconMenu[0].addEventListener('click',()=>{
+			//Efeito de opacidade.
+			
+			let menu = document.getElementById('menu2');
+			if(menu.classList.contains('hide')){
+				menu.classList.add('show');
+				menu.classList.remove('hide');
+			}else{
+				menu.classList.add('hide');
+				menu.classList.remove('show');
+			}
+
+});
+
